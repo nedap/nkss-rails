a31b7c8ed255a84d9a80496675e599d3bbfbf316
Steven Weller <github@suweller.com>
2013-07-09 17:16:44 +0200

    Meh!

--- app/helpers/styleguide_helper.rb
+++ app/helpers/styleguide_helper.rb
@@ -128,8 +128,9 @@ module StyleguideHelper
     lines = lines[line, lines.length]  # ignore anything from before the call
     [].tap do |content|
       while current_line = lines.shift
-        break if current_line.strip.blank? &&
-          current_line.index(/[^ ]/) <= indent
+        if current_line.strip.present?
+          break if current_line.index(/[^ ]/) <= indent
+        end
         content << current_line
       end
     end.join("\n")
